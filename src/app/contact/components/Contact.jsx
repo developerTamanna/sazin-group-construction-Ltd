7fbeb2e2b54a8548747008fb097b65406879bd95
developerTamanna <astamanna57@gmail.com>
2025-08-22 23:03:28 +0600

    contac and project gallery component create

---
+++ src/app/contact/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <header>
+      <div>
+        <h1 className=' text-5xl'>this is a contact page</h1>
+      </div>
+  </header>
+  );
+};
+
+export default Contact;
---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import Contact from './components/Contact';
+const page = () => {
+  return (
+    <header>
+      <div>
+        <Contact></Contact>
+      </div>
+    </header>
+  );
+};
+
+export default page;
