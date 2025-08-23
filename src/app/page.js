9e99c11580b9cfdf11b256128565e5b2fa7e3ab7
developerTamanna <astamanna57@gmail.com>
2025-08-23 20:29:29 +0600

    Achievement and AchievementDetail

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
-import Banner from "./components/Banner";
-import FeatureProducts from "./components/FeatureProducts";
-import LatestNews from "./components/LatestNews";
+import ShortContact from '../components/ShortContact';
+import Achievement from './components/Achievement';
+import Banner from './components/Banner';
+import FeatureProducts from './components/FeatureProducts';
+import LatestNews from './components/LatestNews';
 export default function Home() {
   return (
     <section>
       <div>
-     <Banner></Banner>
-     <FeatureProducts></FeatureProducts>
-     <LatestNews></LatestNews>
+        <Banner></Banner>
+        <FeatureProducts></FeatureProducts>
+        <LatestNews></LatestNews>
+        <Achievement></Achievement>
+        <ShortContact></ShortContact>
       </div>
     </section>
-
   );
 }
---
+++ src/components/ShortContact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShortContact = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-red-600'>hi i m short contact</h1>
+    </div>
+  );
+};
+
+export default ShortContact;
