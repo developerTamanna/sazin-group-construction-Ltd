21a0c76161a9395cebf02c95f4e77241fd831389
MaRuF2131 <marufahmmed916@gmail.com>
2025-08-24 22:24:35 +0600

    padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -284,11 +284,11 @@ export default function Navbar() {
               </li>
 
               {/* More Details Hover */}
-              <li className="relative group">
+              <li className="relative group py-2">
                 <span className="text-gray-700 dark:text-gray-200 hover:text-red-600 cursor-pointer ">
                   More Details
                 </span>
-                <ul className="absolute hidden group-hover:block top-full right-0 bg-white dark:bg-gray-800 shadow-lg mt-2 w-60 rounded-md p-2 space-y-2 z-50 opacity-0 group-hover:opacity-100 transition-all duration-200">
+                <ul className="absolute hidden group-hover:block top-full right-0 bg-white dark:bg-gray-800 shadow-lg w-60 rounded-md p-2 space-y-2 z-50 opacity-0 group-hover:opacity-100 transition-all duration-200">
                   <li>
                     <Link
                       href="/projects"
