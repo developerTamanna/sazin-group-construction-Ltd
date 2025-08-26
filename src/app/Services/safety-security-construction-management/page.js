ed83ec51e76fc1bcff64d01c50f405b5ba849778
MaRuF2131 <marufahmmed916@gmail.com>
2025-08-26 22:34:12 +0600

    resolve

--- src/app/Services/safety-security-construction-management/page.js
+++ src/app/Services/safety-security-construction-management/page.js
@@ -9,7 +9,7 @@ import ContactSafety from "./components/ContactSafety";
 export const metadata = {
   title:"Safety & Security Construction and Management | Sazin Construction Ltd.",
   description:
-  "Specialized solutions in fire safety, security systems, access control, surveillance, and integrated safety management.",
+    "Specialized solutions in fire safety, security systems, access control, surveillance, and integrated safety management.",
 };
 
 export default function SafetyPage() {
